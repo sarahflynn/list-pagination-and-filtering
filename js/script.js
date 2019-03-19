5dc9f3dda287e040cdb09b8ff39285c37f5a74e9
Sarah Flynn <sarah.flynn@icloud.com>
2019-03-19 06:53:08 -0700

    all required pagination functionality working

--- js/script.js
+++ js/script.js
@@ -1,50 +1,43 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
+// global variables
+const students = document.querySelectorAll('.student-item');
+const pagination = document.querySelector('.pagination');
+
+// add and remove classes to toggle student visibility
+function showPage(pageNumber) {
+   const firstStudentIndex = pageNumber * 10 - 10;
+
+   for(let i = 0; i < students.length; i++) {
+      if (i >= firstStudentIndex && i < (firstStudentIndex + 10)) {
+         students[i].classList.add('visible');
+         students[i].classList.remove('hidden');
+
+      } else {
+         students[i].classList.remove('visible');
+         students[i].classList.add('hidden');
+      }
+   }
+}
+
+// create and append page link buttons
+function appendPageLinks() {
+   const numberOfPages = Math.ceil(students.length / 10);
+   let html = '';
    
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
+   for(let i = 0; i < numberOfPages; i++) {
+      html += `<button className='page-link' onclick="showPage(${i + 1})">${i + 1}</button>`
+   }
+
+   const pageNav = document.createElement('div');
+   pageNav.classList.add('page-nav');
+   pageNav.innerHTML = html;
+   pagination.appendChild(pageNav);
+}
 
 
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
+// call showPage() and appendPageLinks() on page load, default to page 1
+window.onload = () => {
+   showPage(1);
+   appendPageLinks();
+}
 
 
-
-
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-
-
-
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.
-***/
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
